c3a25bba19f9872992cb122ff6accf5f2ab13115
chigichan24 <kazuki.chigichan@gmail.com>
2017-07-09 15:35:02 +0900

    インプットを管理するクラスの作成

---
+++ input_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by KazukiChigita on 2017/07/09.
+//
+
+#include "input_manager.h"
---
+++ input_manager.h
@@ -0,0 +1,14 @@
+//
+// Created by KazukiChigita on 2017/07/09.
+//
+
+#ifndef TREE_C_INPUT_MANAGER_H
+#define TREE_C_INPUT_MANAGER_H
+
+
+class input_manager {
+
+};
+
+
+#endif //TREE_C_INPUT_MANAGER_H
