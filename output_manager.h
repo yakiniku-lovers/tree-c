a635078251bfdd0b552d20a0cc222fc0e3a85569
chigichan24 <kazuki.chigichan@gmail.com>
2017-07-09 15:22:25 +0900

    アウトプットを管理するクラスを作成

---
+++ output_manager.h
@@ -0,0 +1,35 @@
+//
+// Created by KazukiChigita on 2017/07/09.
+//
+
+#ifndef TREE_C_OUTPUT_MANAGER_H
+#define TREE_C_OUTPUT_MANAGER_H
+
+
+#include <utility>
+#include <iostream>
+
+class output_manager {
+public:
+    output_manager();
+    ~output_manager();
+    void output_result();
+private:
+    class color{
+    public:
+        color();
+        color(int r,int g,int b);
+        inline int get_r() { return r; }
+        inline int get_g() { return g; }
+        inline int get_b() { return b; }
+
+    private:
+        int r,g,b;
+    };
+    int petal;
+    color col;
+    std::pair<int, color> get_form();
+};
+
+
+#endif //TREE_C_OUTPUT_MANAGER_H
