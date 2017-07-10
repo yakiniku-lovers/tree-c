85254ec3b951a5142c039cc0b4ae19c6edd4cf48
Pelkira <p.true.next@gmail.com>
2017-07-10 12:05:52 +0900

    PETAL_SIZE,SHAPE_SIZEの定義

---
+++ h/constant.h
@@ -0,0 +1,13 @@
+//
+// Created by Shohei Matsugu on 2017/07/10.
+//
+
+#ifndef TREE_C_CONSTANT_H
+#define TREE_C_CONSTANT_H
+
+namespace constant{
+    const int PETAL_SIZE = 13;
+    const int SHAPE_SIZE = 7;
+}
+
+#endif //TREE_C_CONSTANT_H
