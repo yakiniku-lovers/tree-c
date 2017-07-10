b209b87370dfbfdcd10f0c81fe707d29f6488aa9
Pelkira <p.true.next@gmail.com>
2017-07-10 11:28:18 +0900

    resolve #14

--- cpp/evaluator.cpp
+++ cpp/evaluator.cpp
@@ -3,18 +3,19 @@
 //
 #include"./../h/evaluator.h"
 #include"./../h/emotion.h"
+#include "../h/output_manager.h"
 #include<iostream>
 #include<vector>
 #include<map>
 
 //とりあえず。空ならカラコン、否なら先頭を返す。
-user_data evaluator::eval(std::vector<user_data> input){
+output_manager evaluator::eval(std::vector<user_data> input){
+    output_manager provisional_output = output_manager();
     try{
         if(input.size() == 0)throw "Exception: input size is 0.";
     }
     catch(char *str){
         std::cerr << str << std::endl;
-        return user_data();
     }
-    return input[0];
+    return provisional_output;
 }
